5bec80010cc2e63b7c765b24d33cb99ac7a10b36
rutul204 <rutulpatel2000@gmail.com>
2024-08-02 14:02:09 -0700

    Adding front end code

---
+++ README.md
@@ -0,0 +1,8 @@
+Developed a Serverless Web Application using AWS services for a ride-hailing service called Wild Rydes !!
+
+## Steps to setup
+
+1) Deploying a static website using AWS Amplify Console by first creating a git repository in CodeCommit and then pushing the site code.
+2) Configure user management for the website using Amazon Cognito.
+3) Create an AWS Lambda function that will persist data to an Amazon DynamoDB table.
+4) Expose the Lambda function via an Amazon API Gateway as a RESTful API that the static site can call.
--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_3NL1ubUPI', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3j96b9qm1g31skvc4pmv62gm86', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'Cognito User Pool Id', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'Client ID', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'Your Region' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://tebpv6d7cd.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
