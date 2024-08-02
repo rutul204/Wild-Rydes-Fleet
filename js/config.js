a2aa33c530fb35b6260f3a7714905798dc0fe96f
rutul204 <rutulpatel2000@gmail.com>
2024-08-02 14:03:22 -0700

    updated config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'Cognito User Pool Id', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'Client ID', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'Your Region' // e.g. us-east-2
+        userPoolId: 'Cognito User Pool Id',
+        userPoolClientId: 'Client ID',
+        region: 'Your Region'
     },
     api: {
-        invokeUrl: 'https://tebpv6d7cd.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'Your Rest Api Endpoint'
     }
 };
